de16f7c57129ca5102baab7ee4fad15f5928cac7
blak0p <martinpujaltea@gmail.com>
2026-06-26 14:20:13 +0200

    fix: remove AXIOM_BUNKER guard from oh-my-posh prompt
    - Remove [ -n "$AXIOM_BUNKER" ] && return from prompt.sh
    - Makes oh-my-posh work inside axiom containers
    - Also replaced real file with symlink to dotfiles source

--- modules/prompt/bashrc.d/prompt.sh
+++ modules/prompt/bashrc.d/prompt.sh
@@ -1,4 +1,3 @@
-[ -n "$AXIOM_BUNKER" ] && return
 # Cambia el 'eval' normal por este que es compatible con ble.sh
 if [[ ${BLE_VERSION-} ]]; then
   eval "$(oh-my-posh init bash --config ~/.config/oh-my-posh/mi_tema.omp.json --no-exit-code)"

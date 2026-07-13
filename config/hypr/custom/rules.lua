2c8a67c17e938d722fb82d585e01c0a314343399
blak0p <martinpujaltea@gmail.com>
2026-07-13 20:53:06 +0200

    revert: remove zen browser config and hyprland rule

--- config/hypr/custom/rules.lua
+++ config/hypr/custom/rules.lua
@@ -1,8 +1,1 @@
--- Reglas para ventanas específicas
--- Forzar opacidad completa en Zen para mantener transparencia
-hl.window_rule({
-    match = { class = "zen-browser" },
-    opaque = false,
-    force_dim_special = 0,
-    dimaround = false,
-})
+

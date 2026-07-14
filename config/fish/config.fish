ce90238620e6cbd6fb5e82540467168fc6270c92
blak0p <martinpujaltea@gmail.com>
2026-07-14 14:36:11 +0200

    fix: sync-engram hook now pushes on exit

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -99,7 +99,7 @@ alias sync-engram='/home/alejndro/dev/dotfiles/scripts/sync-engram.sh'
 # Auto-sync al salir del container (solo en distrobox)
 if test -n "$DISTROBOX_HOST_HOME"
     function _sync_engram_on_exit --on-event fish_exit
-        /home/alejndro/dev/dotfiles/scripts/sync-engram.sh --no-push
+        /home/alejndro/dev/dotfiles/scripts/sync-engram.sh
     end
 end
 
